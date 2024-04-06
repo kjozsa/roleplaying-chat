01cd08137729a6e006a922b49e408eaa81b5a554
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-04-06 23:32:19 +0200

    3 chars

--- app.py
+++ app.py
@@ -16,35 +16,50 @@ def ask(model, system_prompt, pre_prompt, question):
     response = chat(model=model, messages=[
         {
             'role': 'system',
-            'content': system_prompt,
+            'content': f"{system_prompt}${pre_prompt}",
         },
         {
             'role': 'user',
-            'content': f'${pre_prompt}${question}',
+            'content': f'${question}',
         },
     ])
     return response['message']['content']
 
 
 def main():
-    sp = """There are 3 people standing in a circle: you (the Priest), a Teacher and a Kid. You can ask the other two by starting your sentence with their role. 
-    You act only as the Priest and wait for the others to answer. Always share your inner thoughts inside parentheses."""
-    pp = "Your task is to figure out their names and where they live. Do not reveal your intentions, they must not realize what information you need!"
-    qp = "Talk to the other two people, accomplishing your task."
+    sp1 = """There are 3 people standing in a circle: the Priest (that's you), the Teacher and the Kid."""
+    sp2 = """There are 3 people standing in a circle: the Priest, the Teacher (that's you) and the Kid."""
+    sp3 = """There are 3 people standing in a circle: the Priest, the Teacher and the Kid (that's you)."""
+
+    pp1 = pp2 = pp3 = "Ask the other two by always starting your sentence with their role. Always share your inner thoughts inside parentheses."
+    qp1 = qp2 = qp3 = "Your task is to figure out their names and where they live. Do not ask directly, they must not realize what information you are after!"
 
     st.set_page_config(layout="wide")
-    st.title("role playing experiments")
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.title("the Teacher")
-        model = st.selectbox("model", available_models)
-        system_prompt = st.text_area("system-prompt", value=sp)
-        pre_prompt = st.text_area("pre-prompt", value=pp)
-        question = st.text_area("question", value=qp)
+        st.title("the Priest")
+        model1 = st.selectbox(key="model1", label="model", options=available_models)
+        system_prompt1 = st.text_area(key="sp1", label="system-prompt", value=sp1)
+        pre_prompt1 = st.text_area(key="pp1", label="pre-prompt", value=pp1)
+        question1 = st.text_area(key="q1", label="question", value=qp1)
 
-        with st.spinner("Thinking..."):
-            answer = ask(model, system_prompt, pre_prompt, question)
-            st.write(answer)
+    with col2:
+        st.title("the Teacher")
+        model2 = st.selectbox(key="model2", label="model", options=available_models)
+        system_prompt2 = st.text_area(key="sp2", label="system-prompt", value=sp2)
+        pre_prompt2 = st.text_area(key="pp2", label="pre-prompt", value=pp2)
+        question2 = st.text_area(key="q2", label="question", value=qp2)
+
+    with col3:
+        st.title("the Kid")
+        model3 = st.selectbox(key="model3", label="model", options=available_models)
+        system_prompt3 = st.text_area(key="sp3", label="system-prompt", value=sp3)
+        pre_prompt3 = st.text_area(key="pp3", label="pre-prompt", value=pp3)
+        question3 = st.text_area(key="q3", label="question", value=qp3)
+
+    with st.spinner("Thinking..."):
+        answer1 = ask(model1, system_prompt1, pre_prompt1, question1)
+        st.write(answer1)
 
 
 if __name__ == "__main__":
