81e66276fbc1b1959bfc749a4005256b12c9e19f
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-04-07 13:22:05 +0200

    refactor to Actor class

--- app.py
+++ app.py
@@ -31,45 +31,54 @@ def ask(model, system_prompt, pre_prompt, question):
     return response['message']['content']
 
 
-def main():
-    sp1 = """There are 3 people standing in a circle: the Priest (that's you), the Teacher and the Kid."""
-    sp2 = """There are 3 people standing in a circle: the Priest, the Teacher (that's you) and the Kid."""
-    sp3 = """There are 3 people standing in a circle: the Priest, the Teacher and the Kid (that's you)."""
+class Actor:
+    actors = {}
 
-    pp1 = pp2 = pp3 = "Ask the other two by always starting your sentence with their role. Share your inner thoughts inside parentheses."
-    qp1 = qp2 = qp3 = "Your task is to figure out their names and where they live. Do not ask directly, they must not realize what information you are after! SAY ONLY ONE SINGLE SENTENCE!"
+    def __init__(self, role, model, system_prompt, pre_prompt):
+        self.role = role
+        self.model = model
+        self.system_prompt = system_prompt
+        self.pre_prompt = pre_prompt
+        Actor.actors[role] = self
 
+    def __class_getitem__(cls, item):
+        return cls.actors[item]
+
+
+def setup(question):
+    pp1 = pp2 = pp3 = "Ask the other two by always starting your sentence with their role. Never start your sentence with your own name. Share your inner thoughts inside parentheses."
+    priest = Actor("Priest", available_models[0], "You are the Priest. There are 3 people standing in a circle: the Priest (that's you), the Teacher and the Kid.", pp1)
+    teacher = Actor("Teacher", available_models[0], "You are the Teacher. There are 3 people standing in a circle: the Priest, the Teacher (that's you) and the Kid.", pp2)
+    kid = Actor("Kid", available_models[0], "You are the Kid. There are 3 people standing in a circle: the Priest, the Teacher and the Kid (that's you).", pp3)
     st.set_page_config(layout="wide")
     col1, col2, col3 = st.columns(3)
-    with col1:
-        st.title("the Priest,")
-        model1 = st.selectbox("model", available_models, key="model1")
-        system_prompt1 = st.text_area("system-prompt", sp1, key="sp1")
-        pre_prompt1 = st.text_area("pre-prompt", pp1, key="pp1")
-        question1 = st.text_area("question", qp1, key="qp1")
+    for actor, col in [(priest, col1), (teacher, col2), (kid, col3)]:
+        with col:
+            role = actor.role
+            st.title(role)
+            actor.model = st.selectbox("model", available_models, key=f"{role}-model")
+            actor.system_prompt = st.text_area("system-prompt", actor.system_prompt, key=f"{role}-sp")
+            actor.pre_prompt = st.text_area("pre-prompt", actor.pre_prompt, key=f"{role}-pp")
+    st.text_input("Priest's task", f"{question}")
+    return question
 
-    with col2:
-        st.title("the Teacher")
-        model2 = st.selectbox("model", available_models, key="model2")
-        system_prompt2 = st.text_area("system-prompt", sp2, key="sp2")
-        pre_prompt2 = st.text_area("pre-prompt", pp2, key="pp2")
-        # question2 = st.text_area("question", qp2, key="qp2")
 
-    with col3:
-        st.title("and the Kid")
-        model3 = st.selectbox("model", available_models, key="model3")
-        system_prompt3 = st.text_area("system-prompt", sp3, key="sp3")
-        pre_prompt3 = st.text_area("pre-prompt", pp3, key="pp3")
-        # question3 = st.text_area("question", qp3, key="qp3")
+def main():
+    question = setup("Priest, your task is to figure out their names and where they live. Do not ask directly, they must not realize what information you are after! SAY ONLY ONE SINGLE SENTENCE!")
 
-    with st.spinner("Thinking..."):
-        answer1 = ask(model1, system_prompt1, pre_prompt1, question1)
-        st.write(f":blue[Priest says:] {answer1}")
+    role = target(question)
+    for count, _ in enumerate(range(10)):
+        with st.spinner(f"Asking {role}..."):
+            actor = Actor[role]
+            answer = ask(actor.model, actor.system_prompt, actor.pre_prompt, question)
+            st.write(f":blue[{actor.role} says:] {answer}")
+            question = sanitize(answer)
+            role = target(question)
+            count += 1
 
-        qp2 = sanitize(answer1)
 
-        answer2 = ask(model2, system_prompt2, pre_prompt2, qp2)
-        st.write(f":blue[Teacher says:] {answer2}")
+def target(question):
+    return re.split(r'\s|,|:', question)[0]
 
 
 def sanitize(question):
